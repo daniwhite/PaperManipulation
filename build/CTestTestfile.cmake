e0edaccdf77a5ed921fe108c221e6a523193c534
Dani White <dmwhite@mit.edu>
2020-11-27 10:16:05 -0500

    Version used for project update

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dani/Documents/mit/6.881/project/PaperManipulation
+# Build directory: /home/dani/Documents/mit/6.881/project/PaperManipulation/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ src/paper_dynamics.cpp
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+#include "btBulletDynamicsCommon.h"
+
+int main(void) { printf("Success\r\n"); }
