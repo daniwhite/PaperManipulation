fea1b2666f39c3dc07a9e43e16b13f19afa32f37
Dani White <dmwhite@mit.edu>
2020-12-01 09:28:19 -0500

    Remove build files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dani/Documents/mit/6.881/project/PaperManipulation
-# Build directory: /home/dani/Documents/mit/6.881/project/PaperManipulation/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
