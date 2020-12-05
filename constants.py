3f4aa689ee7f81ef50e4fa8aaf1e73c8a838d255
Dani White <dmwhite@mit.edu>
2020-12-05 17:07:31 -0500

    Move finger stuff to separate file

--- constants.py
+++ constants.py
@@ -1,3 +1,10 @@
+import numpy as np
+
 EPSILON = 0.001
 IN_TO_M = 0.0254
 FRICTION = 0.5
+
+# Fingers normally are around 2cm wide
+FINGER_RADIUS = 0.01
+FINGER_VOLUME = (4/3)*FINGER_RADIUS**3*np.pi
+FINGER_MASS = FINGER_VOLUME*1e3  # Assume finger is made of water
---
+++ finger.py
@@ -0,0 +1,82 @@
+import constants
+import pydrake
+from pydrake.all import RigidTransform, RotationMatrix
+from pydrake.multibody.tree import SpatialInertia, UnitInertia
+import numpy as np
+
+
+def AddFinger(plant, init_x, init_z):
+    """Adds the manipulator."""
+    radius = constants.FINGER_RADIUS
+    finger = plant.AddModelInstance("finger")
+
+    # Add false body at the origin that the finger
+    false_body = plant.AddRigidBody("false_body", finger,
+                                    SpatialInertia(0, [0, 0, 0], UnitInertia(0, 0, 0)))
+
+    # Initialize finger body
+    finger_body = plant.AddRigidBody("body", finger,
+                                     pydrake.multibody.tree.SpatialInertia(
+                                         mass=constants.FINGER_MASS,
+                                         p_PScm_E=np.array([0., 0., 0.]),
+                                         G_SP_E=pydrake.multibody.tree.UnitInertia(1.0, 1.0, 1.0)))
+
+    # Register geometry
+    shape = pydrake.geometry.Sphere(radius)
+    if plant.geometry_source_is_registered():
+        plant.RegisterCollisionGeometry(
+            finger_body, RigidTransform(), shape, "body", pydrake.multibody.plant.CoulombFriction(
+                constants.FRICTION, constants.FRICTION))
+        plant.RegisterVisualGeometry(
+            finger_body, RigidTransform(), shape, "body", [.9, .5, .5, 1.0])
+
+    # Add control joins for x and z movement
+    finger_x = plant.AddJoint(pydrake.multibody.tree.PrismaticJoint(
+        "finger_x",
+        plant.world_frame(),
+        plant.GetFrameByName("false_body"), [1, 0, 0], -1, 1))
+    plant.AddJointActuator("finger_x", finger_x)
+    finger_x.set_default_translation(init_x)
+    finger_z = plant.AddJoint(pydrake.multibody.tree.PrismaticJoint(
+        "finger_z",
+        plant.GetFrameByName("false_body"),
+        plant.GetFrameByName("body"), [0, 0, 1], -1, 1))
+    finger_z.set_default_translation(init_z)
+    plant.AddJointActuator("finger_z", finger_z)
+
+    return finger
+
+
+class PDFinger(pydrake.systems.framework.LeafSystem):
+    """Set up PD controller for finger."""
+
+    def __init__(self, plant, traj):
+        pydrake.systems.framework.LeafSystem.__init__(self)
+        self._plant = plant
+
+        self.DeclareVectorInputPort(
+            "finger_state", pydrake.systems.framework.BasicVector(4))
+        self.DeclareVectorOutputPort(
+            "finger_actuation", pydrake.systems.framework.BasicVector(2),
+            self.CalcOutput)
+
+        self.xs, self.zs, self.xdots, self.zdots = traj
+        self.kx = 5
+        self.kz = 5
+        self.dx = 0.01
+        self.dz = 0.01
+        self.idx = 0
+
+    def CalcOutput(self, context, output):
+        g = self._plant.gravity_field().gravity_vector()[[0, 2]]
+        x, z, xdot, zdot = self.get_input_port(0).Eval(context)
+        if self.idx < len(self.xs):
+            fx = self.kx*(self.xs[self.idx] - x) + \
+                self.dx*(self.xdots[self.idx] - xdot)
+            fz = self.kz*(self.zs[self.idx] - z) + \
+                self.dz*(self.zdots[self.idx] - zdot)
+        else:
+            fx = self.kx*(self.xs[-1] - x) + self.dx*(-xdot)
+            fz = self.kz*(self.zs[-1] - z) + self.dz*(- zdot)
+        output.SetFromVector(-constants.FINGER_MASS*g + [fx, fz])
+        self.idx += 1
