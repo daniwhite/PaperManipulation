749914a4ff33b960c08d30a2fc3e8c48b4585a29
Dani White <dmwhite@mit.edu>
2021-03-22 11:21:40 -0400

    Compensate for gravity

---
+++ arm.py
@@ -0,0 +1,84 @@
+"""Functions for creating and controlling the finger manipulator."""
+
+# Standard imports
+import constants
+import pedestal
+from pydrake.multibody.tree import SpatialInertia, UnitInertia, JacobianWrtVariable
+from pydrake.all import BasicVector
+import numpy as np
+
+# Drake imports
+import pydrake
+from pydrake.all import FindResourceOrThrow, RigidTransform, RotationMatrix
+
+
+class ArmForceController(pydrake.systems.framework.LeafSystem):
+    """Base class for implementing a controller at the finger."""
+
+    def __init__(self, plant):
+        pydrake.systems.framework.LeafSystem.__init__(self)
+
+        self.plant = plant
+        self.arm_instance = self.plant.GetModelInstanceByName("panda")
+        # TODO: fix
+        self.nq_arm = 7  # self.plant.num_positions(arm_instance)
+        self.finger_body = self.plant.GetBodyByName("finger_body")
+
+        self.DeclareVectorInputPort("q", BasicVector(self.nq_arm*2))
+        # self.DeclareAbstractInputPort(
+        #     "poses", pydrake.common.value.AbstractValue.Make([RigidTransform(), RigidTransform()]))
+        # self.DeclareAbstractInputPort(
+        #     "vels", pydrake.common.value.AbstractValue.Make([SpatialVelocity(), SpatialVelocity()]))
+        # self.DeclareAbstractInputPort(
+        #     "contact_results",
+        #     pydrake.common.value.AbstractValue.Make(ContactResults()))
+        self.DeclareVectorOutputPort(
+            "arm_actuation", pydrake.systems.framework.BasicVector(
+                self.nq_arm),
+            self.CalcOutput)
+
+        self.debug = {}
+        self.debug['times'] = []
+
+    def post_finalize_steps(self):
+        self.plant_context = self.plant.CreateDefaultContext()
+
+    # , poses, vels, contact_point, slip_speed, pen_depth, N_hat):
+    def GetForces(self):
+        """
+        Should be overloaded to return [Fy, Fz] to move manipulator (not including gravity
+        compensation.)
+        """
+        # raise NotImplementedError()
+        return np.array([[0, 0]]).T
+
+    def CalcOutput(self, context, output):
+        # This input put is already restricted to the arm, but it includes both q and v
+        q = self.get_input_port().Eval(context)[:self.nq_arm]
+        self.plant.SetPositions(self.plant_context, self.arm_instance, q)
+
+        J_raw = self.plant.CalcJacobianTranslationalVelocity(
+            self.plant_context,
+            JacobianWrtVariable.kQDot,
+            self.finger_body.body_frame(),
+            [0, 0, 0],
+            self.plant.world_frame(),
+            self.plant.world_frame())
+
+        # # finger_instance = self.plant.GetModelInstanceByName("finger") # Finger has no actuation ports
+        # J = J_raw[:, -self.nq_arm:]
+        # J_yz = J[1:]
+        # # J_yz = np.zeros((2, 7))
+
+        # assert J.shape[1] == self.nq_arm
+
+        # forces = self.GetForces()
+        # out = J_yz.T@forces
+
+        out = np.zeros((7, 1))
+        out = out.flatten()
+
+        grav = self.plant.CalcGravityGeneralizedForces(
+            self.plant_context)[-self.nq_arm:]
+        out -= grav
+        output.SetFromVector(out)
