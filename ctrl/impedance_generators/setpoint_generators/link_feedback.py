9ee05cc392b8fb5eb4e66b2bc8700fe7dc26409e
dmwhite <dmwhite@mit.edu>
2022-01-12 11:16:05 -0500

    Add link feedback

---
+++ ctrl/impedance_generators/setpoint_generators/link_feedback.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+from ctrl.common import SystemConstants
+import plant.manipulator
+import plant.pedestal
+
+# Drake imports
+import pydrake
+from pydrake.all import Meshcat, RigidTransform, RollPitchYaw, RotationMatrix
+
+class LinkFeedbackSetpointGenerator(pydrake.systems.framework.LeafSystem):
+    """
+    Generates an impedance trajectory relative to the edge of the link.
+    joint.
+    """
+    def __init__(self):
+        pydrake.systems.framework.LeafSystem.__init__(self)
+
+        # =========================== DECLARE INPUTS ==========================
+        self.DeclareVectorInputPort(
+            "pose_L_rotational", pydrake.systems.framework.BasicVector(3))
+        self.DeclareVectorInputPort(
+            "pose_L_translational", pydrake.systems.framework.BasicVector(3))
+
+        # ========================== DECLARE OUTPUTS ==========================
+        self.DeclareVectorOutputPort(
+            "x0",
+            pydrake.systems.framework.BasicVector(6),
+            self.calc_x0
+        )
+        self.DeclareVectorOutputPort(
+            "dx0",
+            pydrake.systems.framework.BasicVector(6),
+            self.calc_dx0
+        )
+
+
+    def calc_x0(self, context, output):
+        x0 = np.zeros(6)
+
+        # Calc X_L_SP, the transform from the link to the setpoint
+        X_L_SP = RigidTransform() # For now, just make them the same
+
+        # Calc X_W_L
+        pose_L_rotational = self.GetInputPort(
+            "pose_L_rotational").Eval(context)
+        pose_L_translational = self.GetInputPort(
+            "pose_L_translational").Eval(context)
+        X_W_L = RigidTransform(
+            p=pose_L_translational,
+            R=RotationMatrix(RollPitchYaw(pose_L_rotational))
+        )
+
+        # Calc X_W_SP
+        X_W_SP = X_W_L.multiply(X_L_SP)
+
+        translation = X_W_SP.translation()
+        rotation = RollPitchYaw(X_W_SP.rotation()).vector()
+
+        x0[:3] = rotation
+        x0[3:] = translation
+        output.SetFromVector(x0)
+
+
+    def calc_dx0(self, context, output):
+        output.SetFromVector(np.zeros(6))
