d20c546cb43802295af244810960ef6d44b665d8
dmwhite <dmwhite@mit.edu>
2022-01-13 14:28:31 -0500

    Finish pedestal

--- plant/pedestal.py
+++ plant/pedestal.py
@@ -8,10 +8,10 @@ from pydrake.multibody.tree import SpatialInertia, UnitInertia
 
 from constants import THICK_PLYWOOD_THICKNESS, PLYWOOD_LENGTH, IN_TO_M
 
-bump_length = 5*IN_TO_M
+bump_z = 5*IN_TO_M
 PEDESTAL_X_DIM = PLYWOOD_LENGTH
 PEDESTAL_Y_DIM = PLYWOOD_LENGTH/2
-PEDESTAL_Z_DIM = PLYWOOD_LENGTH + bump_length
+PEDESTAL_Z_DIM = PLYWOOD_LENGTH + bump_z
 
 def AddPedestal(plant):
     """
@@ -40,14 +40,61 @@ def AddPedestal(plant):
                                 PEDESTAL_Z_DIM)
         ))
 
-        # if plant.geometry_source_is_registered():
+        if plant.geometry_source_is_registered():
+            plant.RegisterCollisionGeometry(
+                body,
+                RigidTransform(),
+                pydrake.geometry.Box(
+                    THICK_PLYWOOD_THICKNESS,
+                    PEDESTAL_Y_DIM,
+                    PLYWOOD_LENGTH
+                ),
+                full_name,
+                pydrake.multibody.plant.CoulombFriction(1, 1)
+            )
+
+            plant.RegisterVisualGeometry(
+                body,
+                RigidTransform(),
+                pydrake.geometry.Box(
+                    THICK_PLYWOOD_THICKNESS,
+                    PEDESTAL_Y_DIM,
+                    PLYWOOD_LENGTH
+                ),
+                full_name,
+                [0.4, 0.4, 0.4, 1])  # RGBA color
+
+            plant.WeldFrames(
+                plant.world_frame(),
+                plant.GetFrameByName(full_name, pedestal_instance),
+                RigidTransform(RotationMatrix(), [
+                    x_position,
+                    0,
+                    PLYWOOD_LENGTH/2+ bump_z
+                ]
+            ))
+    
+    # Add bump at the bottom
+    full_name = "pedestal_bottom_body"
+    body =  plant.AddRigidBody(
+        full_name,
+        pedestal_instance,
+        SpatialInertia(mass=1, # Doesn't matter because it's welded
+                        p_PScm_E=np.array([0., 0., 0.]),
+                        G_SP_E=UnitInertia.SolidBox(
+                            PEDESTAL_X_DIM,
+                            PEDESTAL_Y_DIM,
+                            bump_z)
+    ))
+
+    if plant.geometry_source_is_registered():
         plant.RegisterCollisionGeometry(
             body,
             RigidTransform(),
             pydrake.geometry.Box(
-                THICK_PLYWOOD_THICKNESS,
+                PEDESTAL_X_DIM,
                 PEDESTAL_Y_DIM,
-                PLYWOOD_LENGTH
+                bump_z
             ),
             full_name,
             pydrake.multibody.plant.CoulombFriction(1, 1)
@@ -57,9 +104,9 @@ def AddPedestal(plant):
             body,
             RigidTransform(),
             pydrake.geometry.Box(
-                THICK_PLYWOOD_THICKNESS,
+                PEDESTAL_X_DIM,
                 PEDESTAL_Y_DIM,
-                PLYWOOD_LENGTH
+                bump_z
             ),
             full_name,
             [0.4, 0.4, 0.4, 1])  # RGBA color
@@ -68,9 +115,9 @@ def AddPedestal(plant):
             plant.world_frame(),
             plant.GetFrameByName(full_name, pedestal_instance),
             RigidTransform(RotationMatrix(), [
-                x_position,
                 0,
-                PLYWOOD_LENGTH/2+ bump_length
+                0,
+                bump_z/2
             ]
         ))
 
