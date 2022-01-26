167fafaa50619352a2951e4463d60600e8ace7eb
dmwhite <dmwhite@mit.edu>
2022-01-26 10:19:01 -0500

    Record end effector frame vis locations
    Still needs cleanup + porting to other instances

--- visualization.py
+++ visualization.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 import pydrake
-from pydrake.geometry import Cylinder, Rgba
+from pydrake.geometry import Cylinder, Rgba, MeshcatAnimation
 from pydrake.math import RigidTransform, RotationMatrix
 from pydrake.all import (
     RigidTransform, RollPitchYaw, RotationMatrix, Meshcat
@@ -14,27 +14,42 @@ def AddMeshcatTriad(meshcat,
                     length=.25,
                     radius=0.01,
                     opacity=1.,
-                    X_PT=RigidTransform()):
+                    X_PT=RigidTransform(),
+                    frame_time=None,
+                    ani=None):
+    if ani is not None:
+        frame = ani.frame(frame_time)
+
     meshcat.SetTransform(path, X_PT)
+    if ani is not None:
+        ani.SetTransform(frame, path, X_PT)
+        
     # x-axis
     X_TG = RigidTransform(RotationMatrix.MakeYRotation(np.pi / 2),
                           [length / 2., 0, 0])
     meshcat.SetTransform(path + "/x-axis", X_TG)
     meshcat.SetObject(path + "/x-axis", Cylinder(radius, length),
-                      Rgba(1, 0, 0, opacity))
+                        Rgba(1, 0, 0, opacity))
+    if ani is not None:
+        ani.SetTransform(frame, path + "/x-axis", X_TG)
+        
 
     # y-axis
     X_TG = RigidTransform(RotationMatrix.MakeXRotation(np.pi / 2),
                           [0, length / 2., 0])
     meshcat.SetTransform(path + "/y-axis", X_TG)
     meshcat.SetObject(path + "/y-axis", Cylinder(radius, length),
-                      Rgba(0, 1, 0, opacity))
+                    Rgba(0, 1, 0, opacity))
+    if ani is not None:
+        ani.SetTransform(frame, path + "/y-axis", X_TG)
 
     # z-axis
     X_TG = RigidTransform([0, 0, length / 2.])
     meshcat.SetTransform(path + "/z-axis", X_TG)
     meshcat.SetObject(path + "/z-axis", Cylinder(radius, length),
-                      Rgba(0, 0, 1, opacity))
+                    Rgba(0, 0, 1, opacity))
+    if ani is not None:
+        ani.SetTransform(frame, path + "/z-axis", X_TG)
 
 class FrameVisualizer(pydrake.systems.framework.LeafSystem):
     """
@@ -45,6 +60,7 @@ class FrameVisualizer(pydrake.systems.framework.LeafSystem):
         pydrake.systems.framework.LeafSystem.__init__(self)
         self.meshcat = meshcat
         self.name = name
+        self.ani = None
 
         # =========================== DECLARE INPUTS ==========================
         self.DeclareVectorInputPort(
@@ -67,4 +83,8 @@ class FrameVisualizer(pydrake.systems.framework.LeafSystem):
 
         X = RigidTransform(p=pos, R=RotationMatrix(RollPitchYaw(rot)))
 
-        AddMeshcatTriad(self.meshcat, self.name, X_PT=X)
+        AddMeshcatTriad(self.meshcat, self.name, X_PT=X, 
+            frame_time=context.get_time(), ani=self.ani)
+    
+    def set_meshcat_to_animation(self, ani: MeshcatAnimation):
+        self.ani = ani
