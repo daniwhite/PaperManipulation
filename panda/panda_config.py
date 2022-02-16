8f5e7a5bd2cfd1ac4be5c72ffff77a1d964c4245
dmwhite <dmwhite@mit.edu>
2022-02-15 20:27:15 -0500

    Regenerate impedeance trajs with new EE location
    Had to lower the angle tolerance slightly, but visually the trajectorystill looks alright.

--- panda/panda_config.py
+++ panda/panda_config.py
@@ -5,7 +5,7 @@ from pydrake.all import RigidTransform
 # Basically, need to move from world frame to Panda frame
 def X_panda_SP_hw(X_W_SP_sw):
     X_panda_SP_sw = X_W_panda.inverse().multiply(X_W_SP_sw)
-    hardware_offset = RigidTransform(p=[0,0,-0.03780])
+    hardware_offset = RigidTransform()
     X_panda_SP_hw = X_panda_SP_sw.multiply(hardware_offset)
     return X_panda_SP_hw
 
